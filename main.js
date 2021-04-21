84115d442684d334b681cfd3bbc01f16f0eb33a5
CharlieS115 <cstorey115@gmail.com>
2021-04-21 16:34:08 +0000

    Added - added gameState variable and given the variable an array of objects for the game start. Ready to be passed in as a parameter

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
+let gameState;
+
 // Code for calling the API
 fetch("https://opentdb.com/api.php?amount=10&category=27&difficulty=easy&type=multiple")
     .then(response => response.json())
     .then(json_data => {
         console.log(json_data);
-        showData(json_data);
+
+        gameState = {
+            questions: loadQuestions(json_data),
+            currentQuestion: -1,
+            score: 0
+        }
+
+        setupGame(gameState);
     });
 
+function setupGame(gameState) {
+
+}
+
 // Function for collecting the data needed from the API 4 answers, the correct answer and the question
-function showData(data) {
+function loadQuestions(data) {
     return data.results
         .map(quest => {
             let answers = quest.incorrect_answers;
