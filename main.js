5d56c17fc1d14fc73d17ff2ebdd7dd8eaf81700f
CharlieS115 <cstorey115@gmail.com>
2021-04-21 16:24:09 +0000

    Added - function for collecting certain data from API added and correct and incorrect answers added to an answers array

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
+// Code for calling the API
 fetch("https://opentdb.com/api.php?amount=10&category=27&difficulty=easy&type=multiple")
     .then(response => response.json())
     .then(json_data => {
         console.log(json_data);
+        showData(json_data);
     });
+
+// Function for collecting the data needed from the API 4 answers, the correct answer and the question
+function showData(data) {
+    return data.results
+        .map(quest => {
+            let answers = quest.incorrect_answers;
+            answers.push(quest.correct_answers);
+            return {
+                question: quest.question,
+                correct_answer: quest.correct_answer,
+                answers
+            };
+        });
+}
