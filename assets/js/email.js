41bea20a1721bdfdbe6aee73c5e5eb2d2b9242f0
CharlieS115 <cstorey115@gmail.com>
2021-04-23 08:43:14 +0000

    Added & Moved - moved email.js to js folder inside assets and update javascript in email.js so emails can be submited and sent via emailjs service

---
+++ assets/js/email.js
@@ -0,0 +1,17 @@
+function sendMail(contactForm) {
+    emailjs.send("service_dwqe96l", "Trivia World Contact", {
+            "first_name": contactForm.firstname.value,
+            "last_name": contactForm.lastname.value,
+            "from_email": contactForm.emailaddress.value,
+            "quiz_idea": contactForm.quizidea.value
+        })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false;
+}
